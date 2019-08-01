0470de17c3043deb792c987be9b39ae0ea44f1b9
Steven Ballard <stevenballardstudios@gmail.com>
2019-08-01 10:27:58 -0700

    Added Async add functionality

--- eCommerce/Data/VideoGameDb.cs
+++ eCommerce/Data/VideoGameDb.cs
@@ -16,10 +16,10 @@ namespace eCommerce.Data
         /// </summary>
         /// <param name="g">The game to be added</param>
         /// <param name="context">The DB context to use</param>
-        public static VideoGame Add(VideoGame g, GameContext context)
+        public static async Task<VideoGame> Add(VideoGame g, GameContext context)
         {
-            context.Add(g);
-            context.SaveChanges();
+            await context.AddAsync(g);
+            await context.SaveChangesAsync();
             return g;
         }
     }
