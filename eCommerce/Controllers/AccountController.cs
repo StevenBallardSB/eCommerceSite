986b6f1a5c41eb028560570979d5bcce26dca89e
steven ballard <stevenballardstudios@gmail.com>
2019-08-08 10:55:57 -0700

    Bagan work on AccountController

---
+++ eCommerce/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Add constructor with DB Context as a parameter
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Member)
+        {
+            // TODO: Add member to database
+            // Display success message on home page
+            // after redirecting
+        }
+    }
+}
