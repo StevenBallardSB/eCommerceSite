ac6622116af33c4c42f0e68be2cbc287482cc661
steven ballard <stevenballardstudios@gmail.com>
2019-08-13 10:57:01 -0700

    Implemented basic login

--- eCommerce/Controllers/AccountController.cs
+++ eCommerce/Controllers/AccountController.cs
@@ -35,5 +35,31 @@ namespace eCommerce.Controllers
 
             return View(m);
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isMember = await MemberDb.IsLoginValid(model, _context);
+                if (isMember)
+                {
+                    TempData["Message"] = "Logged in successfully";
+                    return RedirectToAction("Index", "Home");
+                }
+                else // Credentials invalid
+                {
+                    // Adding model error with no key, will display error message in the validation summary
+                    ModelState.AddModelError(string.Empty, "Username or Password is incorrect");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
