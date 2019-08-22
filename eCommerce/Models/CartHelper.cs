512f1c246917d8c35de413ae644a5efd50dbfdf2
steven ballard <stevenballardstudios@gmail.com>
2019-08-22 09:41:04 -0700

    Added cart helper class

---
+++ eCommerce/Models/CartHelper.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Contians helper methods to manage the users shopping cart
+    /// </summary>
+    public class CartHelper
+    {
+        private const string CartCookie = "Cart";
+        /// <summary>
+        /// Gets the current user VideoGames from their shopping car. If there are no games, an aempty list is returned
+        /// </summary>
+        /// <param name="accessor"></param>
+        /// <returns></returns>
+        public static List<VideoGame> GetGames(IHttpContextAccessor accessor)
+        {
+            //Get data out of cookie
+            string data = accessor.HttpContext.Request.Cookies[CartCookie];
+
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return new List<VideoGame>();
+            }
+
+            List<VideoGame> games = JsonConvert.DeserializeObject<List<VideoGame>>(data);
+
+            return games;
+        }
+
+        /// <summary>
+        /// Get total number of video games in the cart
+        /// </summary>
+        /// <param name="accessor"></param>
+        /// <returns></returns>
+        public static int GetGameCount(IHttpContextAccessor accessor)
+        {
+            List<VideoGame> allGames = GetGames(accessor);
+            return allGames.Count;
+        }
+
+
+        /// <summary>
+        ///  Adds VideoGame to the cart
+        /// </summary>
+        /// <param name="accessor">accessor</param>
+        /// <param name="g">Video game to be added</param>
+        public static void Add(IHttpContextAccessor accessor, VideoGame g)
+        {
+            List<VideoGame> games = GetGames(accessor);
+            games.Add(g);
+
+            string data = JsonConvert.SerializeObject(games);
+
+            accessor.HttpContext.Response.Cookies.Append(CartCookie, data);
+        }
+    }
+}
