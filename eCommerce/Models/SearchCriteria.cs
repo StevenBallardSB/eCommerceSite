07350c187fa857794f5ff33904799ca0a1720b26
steven ballard <stevenballardstudios@gmail.com>
2019-08-29 09:44:41 -0700

    Created SearchCriteria properties

---
+++ eCommerce/Models/SearchCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class SearchCriteria
+    {
+        public string Title { get; set; }
+
+        public string Rating { get; set; }
+
+        public double? minPrice { get; set; }
+
+        public double? MaxPrice { get; set; }
+    }
+}
